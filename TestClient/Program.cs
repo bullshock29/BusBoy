7afc80f26054d4e1969ad67395dc2e594b9cca9d
bullshock29 <bullshock29@hotmail.com>
2023-08-01 10:21:24 -0400

    Basic test clients added Stub for async added

---
+++ TestClient/Program.cs
@@ -0,0 +1,5 @@
+﻿BusBoy.RabbitMQ.RPC.RpcClient client = new BusBoy.RabbitMQ.RPC.RpcClient("localhost");
+string ret = await client.CallAsync("hello world");
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadLine();
---
+++ TestServer/Program.cs
@@ -0,0 +1,8 @@
+﻿using TestServer;
+
+BusBoy.RabbitMQ.RPC.RpcServer server = new BusBoy.RabbitMQ.RPC.RpcServer();
+Core core = new Core();
+server.Start("localHost", core.DoWork);
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadLine();
